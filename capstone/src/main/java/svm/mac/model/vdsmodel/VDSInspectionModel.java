5cb19199a38fbba3ff80d9b9f074eadd5469e785
MohanSaiPK <86645629+MohanSaiPK@users.noreply.github.com>
2026-01-09 14:34:02 +0530

    Add files via upload

---
+++ capstone/src/main/java/svm/mac/model/vdsmodel/VDSInspectionModel.java
@@ -0,0 +1,26 @@
+package svm.mac.model.vdsmodel;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import svm.mac.model.ONBInspectionInsert;
+import svm.mac.model.ONBInspectionRowGet;
+
+public class VDSInspectionModel implements Serializable {
+
+    @SerializedName("onbinspection")
+    @Expose
+    private List<ONBInspectionRowGet> onbinspection = new ArrayList<>();
+
+    public List<ONBInspectionRowGet> getOnbinspection() {
+        return onbinspection;
+    }
+
+    public void setOnbinspection(List<ONBInspectionRowGet> onbinspection) {
+        this.onbinspection = onbinspection;
+    }
+}
